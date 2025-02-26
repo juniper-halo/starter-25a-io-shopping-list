3f5631ff9501678a6889ff5fde395bc6ebbe7848
michael <mnowak1@illinois.edu>
2025-02-26 14:42:00 -0600

    init

--- Makefile
+++ Makefile
@@ -1,14 +1,10 @@
 CXX=clang++
-CXX_FLAGS=-std=c++20 -Iincludes -Wall -Wextra -O0 -g
+CXX_FLAGS=-std=c++20 -I ./includes -Wall -Wextra -Werror -Wno-unused-parameter -pedantic -O0 -g
 
 exec: bin/exec
-tests: bin/tests
 
-bin/exec: ./src/driver.cc ./includes/student.hpp ./includes/solution.hpp ./src/solution.cc
+bin/exec: ./src/driver.cc ./includes/item.hpp ./includes/solution.hpp ./src/solution.cc
 	$(CXX) $(CXX_FLAGS) ./src/driver.cc ./src/solution.cc -fsanitize=address -o $@
-
-bin/tests: ./tests/tests.cc ./includes/student.hpp ./includes/solution.hpp ./src/solution.cc
-	$(CXX) $(CXX_FLAGS) -Itests ./tests/tests.cc ./src/solution.cc -o $@
 
 .DEFAULT_GOAL := exec
 .PHONY: exec clean
--- src/driver.cc
+++ src/driver.cc
@@ -1,8 +1,27 @@
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <vector>
 
 #include "solution.hpp"
 
 int main() {
-  // todo
+  try {
+    std::string test_input_file = "./tests/test-listOne.txt";
+    std::vector<Item> shopping_items =
+        LoadShoppingItemsFromFile(test_input_file);
+
+    std::cout << "Printing shopping items...\n";
+    for (const Item& item : shopping_items) {
+      std::cout << "\t" << item.item_name << " " << item.quantity << " "
+                << item.price << "\n";
+    }
+    std::cout << "Printing complete!\n";
+
+  } catch (const std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return 1;
+  }
+
+  return 0;
 }
